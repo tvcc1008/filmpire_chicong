836ce11fdab61fdc9d25876e1cdbe45fab66adca
tvcc1008 <tvcc.1008@gmail.com>
2022-08-03 15:19:34 +0700

    Rendering Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..'
 
 const Movies = () => {
-    console.log('Movies')
+  const { data, error, isFetching } = useGetMoviesQuery()
+  console.log(data)
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    )
+  }
+
+  if(!data.results.length){
+    return(
+      <Box display='flex' justifyContent='center' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    )
+  }
+
+  if(error) return 'An error has occured.'
+
   return (
-    <div>Movies</div>
+    <MovieList movies={data}>
+
+    </MovieList>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
+import store from './app/store';
+
+const theme = createTheme();
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>,
     document.getElementById('root')
 );
